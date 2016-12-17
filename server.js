2cf575aee810287fa3b6511ecdf42655ee3e1350
doerschera <doerschera@gmail.com>
2016-12-17 12:51:58 -0600

    set port for heroku

--- server.js
+++ server.js
@@ -26,6 +26,4 @@ app.use('/', routes);
 
 
 // ------- localhost server -------------
-app.listen(3000, function() {
-  console.log('Running on port 3000');
-})
+app.listen(process.env.PORT || 3000);
